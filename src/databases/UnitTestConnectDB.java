c83796135c884e12d091b54fea7d4c4696316a8b
“NaimKhanSiam” <naim_khan@live.co.uk>
2018-04-08 13:12:05 -0400

    1.11pm,Day-1

---
+++ src/databases/UnitTestConnectDB.java
@@ -0,0 +1,9 @@
+package databases;
+
+import org.testng.Assert;
+
+public class UnitTestConnectDB {
+    public static void main(String[] args) {
+        ConnectDB connectDB = new ConnectDB();
+    }
+}
---
+++ src/design/AbstractEmployee.java
@@ -0,0 +1,9 @@
+package design;
+
+public abstract class AbstractEmployee {
+
+    public void breakTime(){
+        System.out.println("employees should get 30 min paid break a day");
+    }
+
+}
